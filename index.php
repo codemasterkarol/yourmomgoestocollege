5738dab89b8586c63c4eafbe210df7be5c131775
codemasterkarol <codemasterkarol@gmail.com>
2017-01-31 17:30:35 -0600

    Added an OG image

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:image" content="screenshot.png">
+    <meta property="og:image" content="ogimage.png"/>
     <title>Your Mom Goes To College!</title>
     <style>
         body {
